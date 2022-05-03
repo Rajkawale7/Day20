b7afdade6e88c96df125e5119bebb4a74071504a
Rajkawale7 <Rajkawale712@gmail.com>
2022-05-03 15:14:01 +0530

    Added: Program case to validate User's email

--- src/com/bridgelabz/UserRegistrationTest.java
+++ src/com/bridgelabz/UserRegistrationTest.java
@@ -2,16 +2,16 @@ package com.bridgelabz;
 import org.junit.Assert;
 import org.junit.Test;
 public class UserRegistrationTest {
-	 @Test
-	    public void givenLastName_WhenProper_ShouldReturn_True() {
-	        UserValidater userValidater = new UserValidater();
-	        boolean result = userValidater.validateLastName("Kawale");
-	        Assert.assertEquals(true,result);
-	    }
-	    @Test
-	    public void givenLastName_WhenNotProper_ShouldReturn_False() {
-	        UserValidater userValidater = new UserValidater();
-	        boolean result = userValidater.validateLastName("Ka");
-	        Assert.assertEquals(false,result);
+	@Test
+    public void givenEmail_WhenProper_ShouldReturn_True() {
+        UserValidater userValidater = new UserValidater();
+        boolean result = userValidater.validateEmail("abc.xyz@bl.co.in");
+        Assert.assertEquals(true,result);
+    }
+    @Test
+    public void givenEmail_WhenNotProper_ShouldReturn_False() {
+        UserValidater userValidater = new UserValidater();
+        boolean result = userValidater.validateEmail("shubham@.com");
+        Assert.assertEquals(false,result);
 	    }
 	}
--- src/com/bridgelabz/UserValidater.java
+++ src/com/bridgelabz/UserValidater.java
@@ -3,9 +3,9 @@ package com.bridgelabz;
 import java.util.regex.Pattern;
 
 public class UserValidater {
-		private static final String NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
-    	Pattern pattern = Pattern.compile(NAME_PATTERN);
-    	public boolean validateLastName(String lastName) {
-        return pattern.matcher(lastName).matches();
+		private static final String EMAIL_PATTERN = "^[a-zA-Z0-9.+_-]+[@][a-zA-Z0-9]+[.]co(m|.in)$";
+		public boolean validateEmail(String email) {
+	        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+	        return pattern.matcher(email).matches();
     }
 }
