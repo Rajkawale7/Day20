fa424c94cabab47750bda8e3596104707e3813e9
Rajkawale7 <Rajkawale712@gmail.com>
2022-05-03 15:08:39 +0530

    Added: Program case to validate User's last name

--- src/com/bridgelabz/UserValidater.java
+++ src/com/bridgelabz/UserValidater.java
@@ -3,10 +3,9 @@ package com.bridgelabz;
 import java.util.regex.Pattern;
 
 public class UserValidater {
-	private static final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
-
-    public boolean validateFirstName(String firstName) {
-        Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
-        return pattern.matcher(firstName).matches();
+		private static final String NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
+    	Pattern pattern = Pattern.compile(NAME_PATTERN);
+    	public boolean validateLastName(String lastName) {
+        return pattern.matcher(lastName).matches();
     }
 }
